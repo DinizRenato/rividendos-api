026e5142b8d151f2ceadd5562e5b70572ce891f6
Renato Diniz <dinizre@hotmail.com>
2021-06-11 10:53:29 -0300

    vUsersService : GetUserByIdService

--- src/modules/users/services/test/GetUserByIdService.spec.ts
+++ src/modules/users/services/test/GetUserByIdService.spec.ts
@@ -1,46 +1,43 @@
 import 'reflect-metadata';
+
 import FakeUsersRepository from '@modules/users/domain/repositories/fakes/FakeUsersRepository';
-import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
-import AppError from '@shared/errors/AppError';
 import CreateUserService from '../CreateUserService';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import GetUserByIdService from '../GetUserByIdService';
+import AppError from '@shared/errors/AppError';
 
 let fakeUsersRepository: FakeUsersRepository;
 let createUserService: CreateUserService;
+let getUserByIdService: GetUserByIdService;
 let hashProvider: FakeHashProvider;
 
-describe('CreateUser', () => {
+describe('GetUserById', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
     hashProvider = new FakeHashProvider();
+    getUserByIdService = new GetUserByIdService(fakeUsersRepository);
     createUserService = new CreateUserService(
       fakeUsersRepository,
       hashProvider,
     );
   });
 
-  it('should be able to create a new user', async () => {
+  it('should be able to get user by id', async () => {
     const user = await createUserService.execute({
       name: 'Renato Diniz',
       email: 'teste@email.com',
       password: '123456',
     });
 
-    expect(user).toHaveProperty('id');
+    const id = user.id;
+
+    const result = await getUserByIdService.execute({ id });
+
+    expect(result).toBe(user);
   });
 
-  it('should not be able to create two users with the same email', async () => {
-    await createUserService.execute({
-      name: 'Renato Diniz',
-      email: 'teste@email.com',
-      password: '123456',
-    });
-
-    expect(
-      createUserService.execute({
-        name: 'Renato Diniz',
-        email: 'teste@email.com',
-        password: '123456',
-      }),
-    ).rejects.toBeInstanceOf(AppError);
+  it('should be able to get error user not found', async () => {
+    const id = 'kwhskdksjvfakjhkahkre';
+    expect(getUserByIdService.execute({ id })).rejects.toBeInstanceOf(AppError);
   });
 });
